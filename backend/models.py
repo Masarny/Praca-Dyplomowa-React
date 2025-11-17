32e6aea6dacc1937a2b386a6b04df21c717fc642
Masarny <susel123@interia.pl>
2025-11-17 15:22:58 +0100

    Documentation v0.45

--- backend/models.py
+++ backend/models.py
@@ -11,7 +11,7 @@ class User(db.Model):
     totp_secret = db.Column(db.String(32), nullable=True)
 
 
-class StoredPasswords(db.Model):
+class StoredPassword(db.Model):
     __tablename__ = "storedpasswords"
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
